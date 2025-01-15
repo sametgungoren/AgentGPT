a7f9736bb8ce97fae11dba96746424df2da4e113
Samet GÜNGÖREN <sametgungoren@hotmail.com>
2025-01-15 21:26:26 +0300

    feat: Add Docker support and update package dependencies

--- setup.bat
+++ setup.bat
@@ -1,7 +1,13 @@
+REM filepath: /setup.bat
 @echo off
 setlocal
 
-rem  The CLI will take care of setting up the ENV variables
+REM The CLI will take care of setting up the ENV variables
 cd cli || exit /b 1
-call npm install
+call npm install --legacy-peer-deps
 npm run start
+
+REM Please go into the ./next folder and run npm install with legacy peer deps
+cd ../next
+call npm install --legacy-peer-deps
+npm run dev
